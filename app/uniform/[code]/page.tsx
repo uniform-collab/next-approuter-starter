22592daff534458dbba2ac7017d2c679be272be8
Alex <alex.shyba@uniform.dev>
2026-02-09 16:02:50 -0800

    feat: configure ISR for UniformPage by adding generateStaticParams function to enable caching on first visit.

--- app/uniform/[code]/page.tsx
+++ app/uniform/[code]/page.tsx
@@ -5,6 +5,11 @@ import {
 } from "@uniformdev/next-app-router";
 import { resolveComponent } from "@/components/resolveComponent";
 
+// Enable ISR: pages are generated on first visit and cached
+export const generateStaticParams = async () => {
+  return [];
+};
+
 export default async function UniformPage(props: UniformPageParameters) {
   const { code } = await props.params;
   return (
