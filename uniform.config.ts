a96468390f8170735dd2b68e6795addc7a13e067
Alex <alex.shyba@uniform.dev>
2026-02-11 17:50:46 -0800

    revert: serialization changes

--- uniform.config.ts
+++ uniform.config.ts
@@ -1,19 +1,3 @@
 import { uniformConfig } from '@uniformdev/cli/config';
-import * as dotenv from 'dotenv';
 
-dotenv.config({ path: `.env.local` });
-
-module.exports = uniformConfig({
-    preset: 'none',
-    config: {
-        serialization: {
-            entitiesConfig: {
-                component: {},
-                composition: { publish: true },
-                componentPattern: {},
-                projectMapNode: {},
-                projectMapDefinition: {},
-            },
-        },
-    },
-});
+module.exports = uniformConfig({ preset: "all", disableEntities: ["webhook", "policyDocument", "previewViewport"] });
