118b24263f8084a45039bcaba6283e31c178ff0c
erzr <aelamarre@gmail.com>
2026-02-25 15:17:57 -0500

    consider sandbox url

--- app/api/preview/route.ts
+++ app/api/preview/route.ts
@@ -3,7 +3,27 @@ import {
   createPreviewPOSTRouteHandler,
   createPreviewOPTIONSRouteHandler,
 } from "@uniformdev/next-app-router/handler";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = createPreviewGETRouteHandler();
+export const GET = async (request: NextRequest) => {
+  const result = await createPreviewGETRouteHandler()(request);
+
+  if (!result) {
+    return new NextResponse(null, { status: 500 });
+  }
+
+  if (result.status === 307 && process.env.SANDBOX_DOMAIN) {
+    const location = result.headers.get("location");
+    const original = new URL(location ?? "/", request.url);
+    const target = new URL(
+      original.pathname + original.search + original.hash,
+      process.env.SANDBOX_DOMAIN
+    );
+    return NextResponse.redirect(target);
+  }
+
+  return result;
+};
+
 export const POST = createPreviewPOSTRouteHandler();
 export const OPTIONS = createPreviewOPTIONSRouteHandler();
