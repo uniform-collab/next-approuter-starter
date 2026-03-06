709cc91437a85291cb74ecc2843b947c050675c3
erzr <aelamarre@gmail.com>
2026-03-06 07:04:50 -0500

    simplify layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,15 +9,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="relative min-h-dvh flex flex-col">
-          <main className="flex-1 flex flex-col">{children}</main>
-          <footer className="py-6 text-center text-xs text-neutral-400 tracking-wide">
-            Built with Uniform &middot; Next.js
-          </footer>
-        </div>
-      </body>
-    </html>
+    <>
+      <body className={inter.className}>{children}</body>
+    </>
   );
 }
